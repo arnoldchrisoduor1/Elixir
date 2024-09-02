4c03938cc40b5ed512c8c351f77e656f9b4d9444
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-09-02 23:44:33 +0300

    lists

--- code/tutorials/lib/lists/lists.ex
+++ code/tutorials/lib/lists/lists.ex
@@ -21,12 +21,14 @@ defmodule Tutorials.Lists do
   def map([], _, acc), do: reverse(acc)
   def map([h | t], func, acc), do: map(t, func, [func.(h) | acc])
 
+
   #--------------------------------- Concat -----------------------------------
   def concat(src, dst), do: concat_func(src |> reverse(), dst)
 
   defp concat_func([], dst), do: dst
   defp concat_func([h | t], dst), do: concat_func(t, [h | dst])
 
+  
   #---------------------------FlatMap ------------------------------------------
   def flat_map(elements, func, acc \\ [])
   def flat_map([], _, acc), do: acc
