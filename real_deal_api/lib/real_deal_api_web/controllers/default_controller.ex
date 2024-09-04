d708bb78330793e7649f107d5615c16e4ab82026
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-09-04 13:18:38 +0300

    delete real deal api

--- real_deal_api/lib/real_deal_api_web/controllers/default_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule RealDealApiWeb.DefaultController do
-  use RealDealApiWeb, :controller
-
-  def index(conn ,_params) do
-    text conn, "The Real Deal API is LIVE - #{Mix.env()}"
-  end
-
-end
